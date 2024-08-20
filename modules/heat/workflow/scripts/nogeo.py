58fae5739abca19c3860416374e70356a201df23
Ivan Ruiz Manuel <72193617+irm-codebase@users.noreply.github.com>
2024-08-20 14:41:55 +0200

    Initial commit of the modular heat workflow
    Co-authored-by: Bryn Pickering <17178478+brynpickering@users.noreply.github.com>
    Co-authored-by: Tim Tröndle <tim.troendle@usys.ethz.ch>
    Co-authored-by: Meijun Chen <m.chen-4@tudelft.nl>
    Co-authored-by: Stefan Pfenninger <stefan@pfenninger.org>
    Co-authored-by: adrienmellot <adrien.mellot@gmail.com>

---
+++ modules/heat/workflow/scripts/nogeo.py
@@ -0,0 +1,22 @@
+import geopandas as gpd
+import pandas as pd
+
+
+def remove_geo_information(path_to_units, path_to_units_without_geo_information):
+    units = gpd.read_file(path_to_units).set_index("id")
+    units = pd.DataFrame(units)
+    del units["geometry"]
+    units.to_csv(path_to_units_without_geo_information, index=True, header=True)
+
+
+# if __name__ == "__main__":
+#     remove_geo_information(
+#         path_to_units=snakemake.input.units,
+#         path_to_units_without_geo_information=snakemake.output[0],
+#     )
+
+if __name__ == "__main__":
+    remove_geo_information(
+        path_to_units=snakemake.input.units,
+        path_to_units_without_geo_information=snakemake.output[0],
+    )
